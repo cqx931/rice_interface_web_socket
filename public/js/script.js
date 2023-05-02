798fce85dfd50ebff0ee7c89301d06407b234062
Alberto Harres <alberto.harres@gmail.com>
2023-05-02 16:58:36 +0200

    receive layers message

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,8 @@
 const socket = io();
+
+// render socket message on div
+socket.on("layers", data => {
+  console.log('socket, layers', data)
+  const div = document.getElementById("message");
+  div.innerHTML = data;
+})
