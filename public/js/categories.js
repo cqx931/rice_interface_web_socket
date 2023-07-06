114b03efa74a545dff2f18c35ed9aee99c80f856
cqx931 <cqx931@gmail.com>
2023-07-07 01:51:52 +0200

    save

--- public/js/categories.js
+++ public/js/categories.js
@@ -8,19 +8,19 @@ const categories = [
     },
     {
       name: "The Delicate Heart",
-      text: ["Warns of fragility and the need for caution and obstacles to be overcome. But you will move through the obstacles like a lizard coming down a tree.", "Turmoil coming from inside will make you feel threatened by the outside. Try to really look at what's in front of you. you're safe.","Many moons ago you hid a treasure. it's still there, waiting for you."],
+      text: ["Warns of fragility and the need for caution and obstacles to be overcome. But you will move through the obstacles like a lizard coming down a tree.", "Turmoil coming from inside will make you feel threatened by the outside. Try to really look at what's in front of you. You're safe.","Many moons ago you hid a treasure. it's still there, waiting for you."],
       detections: [],
       f: (s) => s == "*" || s == "*º" || s == "º*", // delicate heart
     },
     {
       name: "Hidden Balance",
-      text: ["Two subtle signs of cracks, a hidden but fragile balance is achieved at the current moment.", "There's much peace to be had in grass. Remember we all come from bacteria."],
+      text: ["Two subtle signs of cracks, a hidden but fragile balance is achieved at the current moment, whispering secrets of vulnerability and resilience.", "There's much peace to be had in grass. Dance with the shadow and let your mind fly with the cloud."],
       detections: [],
       f: (s) => s.count('*') == 2 && s.count('_') == 0 && s.count('|') == 0, // hiddem balance
     },
     {
       name: "The Mist",
-      text: ["No clear sign cracks with uneven surface. The destiny is not set, everything is possible."],
+      text: ["No clear sign of cracks with uneven surface. On top of the hill, the clouds are still cooking the rice."],
       detections: [],
       f: (s) => s == "", // The Mist -  empty
     },
@@ -32,19 +32,19 @@ const categories = [
     },
     {
       name: "The Libra",
-      text: ["Two subtle signs of cracks joined by one clear divider crack, showing a struggle between two things."],
+      text: ["Two subtle signs of cracks joined by one clear divider crack, unveil a struggle between two realms."],
       detections: [],
       f: (s) => s.count('*') == 2 && s.count('_') == 1, // libra
     },
     {
       name: "The Divider",
-      text: ["There's a path, and the choice to follow it is yours.", "Far away a country was split in two pieces, like a chocolate bar. you may wish it didn't, but often history repeats itself.", "A clear horizontal crack the separate the grain into two almost identical halves, an indicator of a clear division that needs to be made."],
+      text: ["A path reveals before you, the choice to follow it is yours. A testament to the boundless potential that resides within your soul.", "Far away a country was split in two pieces, like a chocolate bar. you may wish it didn't, but often history repeats itself.", "A clear horizontal crack the separate the grain into two halves, an indicator of a clear division that needs to be made."],
       detections: [],
       f: (s) => s.count('_') == 1 && s.count('|') == 0, // divider
     },
     {
       name: "Mirror",
-      text: ["Even inanimate objects change and grow. watch a rock with your fingers until you understand its dance.", "Two cracks interact with each other. Everything that you touch touches you back. Let life leave marks on you.", "Two cracks are distributed on the surface of the rice grain. Every person you meet has a unique story and perspective. Listen to them with an open heart and you may learn something new."],
+      text: ["Two cracks interact with each other. Everything that you touch touches you back. Let life leave marks on you.", "Two cracks are distributed on the surface of the rice grain. Every person you meet has a unique story and perspective. Listen to them with an open heart and you may learn something new.", "Even inanimate objects change and grow. watch a rock with your fingers until you understand its dance."],
       detections: [],
       f: (s) => s.count('_') == 2 && s.count('|') == 0, // the mirror
     },
@@ -55,16 +55,10 @@ const categories = [
       f: (s) => s.count('_') == 3 && s.count('|') == 0, // The Three Parallels
     },
     {
-      name: "Four Parallels",
-      text: ["A grain is a seed. A grain is a map. You contain inside you guides for the many versions of yourself you could be.","The light and the sharp and the soft and the porous share a space. In the middle of the road a few things become clear."],
+      name: "The Multiverse",
+      text: ["A grain is a seed. A grain is a map. You contain inside you guides for the many versions of yourself you could be.","Multiple cracks tightly packed on a single piece of rice, speaks of a crowded and bustling time to come."],
       detections: [],
-      f: (s) => s.count('_') == 4 && s.count('|') == 0, // Four cracks
-    },
-    {
-      name: "The Clustered Five",
-      text: ["Tightly packed on a single piece of rice, speaks of a crowded and bustling time to come.", "You will be ask to count. do it wholeheartedly, like you believe in numbers."],
-      detections: [],
-      f: (s) => s.count('_') == 5, // The Clustered Five
+      f: (s) => s.count('_') >= 4 && s.count('|') == 0, // Four cracks
     },
     {
       name: "Curvy Lines",
