84c442ffbd6c4671273d843b57f7cd2cd3bda2a9
cqx931 <cqx931@gmail.com>
2023-07-07 02:03:26 +0200

    match with print

--- public/js/categories.js
+++ public/js/categories.js
@@ -4,25 +4,25 @@ const categories = [
       name: "Branches",
       text: ["A vertical crack cut across multiple horizontal cracks in the middle of the surface, foretells a journey filled with many paths and possibilities.","Two or multiple lines meets, speaks of unexpected allies and the coming together of forces."],
       detections: [],
-      f: (s) => s.count('|') == 1 && s.count('+') >= 1, // branches
+      f: (s) => s.count('|') == 1 && s.count('+') >= 1, // Branches
     },
     {
       name: "The Delicate Heart",
       text: ["Warns of fragility and the need for caution and obstacles to be overcome. But you will move through the obstacles like a lizard coming down a tree.", "Turmoil coming from inside will make you feel threatened by the outside. Try to really look at what's in front of you. You're safe.","Many moons ago you hid a treasure. it's still there, waiting for you."],
       detections: [],
-      f: (s) => s == "*" || s == "*º" || s == "º*", // delicate heart
+      f: (s) => s == "*" || s == "*º" || s == "º*", // The Delicate heart
     },
     {
       name: "Hidden Balance",
       text: ["Two subtle signs of cracks, a hidden but fragile balance is achieved at the current moment, whispering secrets of vulnerability and resilience.", "There's much peace to be had in grass. Dance with the shadow and let your mind fly with the cloud."],
       detections: [],
-      f: (s) => s.count('*') == 2 && s.count('_') == 0 && s.count('|') == 0, // hiddem balance
+      f: (s) => s.count('*') == 2 && s.count('_') == 0 && s.count('|') == 0, // Hiddem Balance
     },
     {
       name: "The Mist",
       text: ["No clear sign of cracks with uneven surface. On top of the hill, the clouds are still cooking the rice."],
       detections: [],
-      f: (s) => s == "", // The Mist -  empty
+      f: (s) => s == "", // The Mist
     },
     {
       name: "The Broken Cloud",
@@ -43,7 +43,7 @@ const categories = [
       f: (s) => s.count('_') == 1 && s.count('|') == 0, // divider
     },
     {
-      name: "Mirror",
+      name: "The Mirror",
       text: ["Two cracks interact with each other. Everything that you touch touches you back. Let life leave marks on you.", "Two cracks are distributed on the surface of the rice grain. Every person you meet has a unique story and perspective. Listen to them with an open heart and you may learn something new.", "Even inanimate objects change and grow. watch a rock with your fingers until you understand its dance."],
       detections: [],
       f: (s) => s.count('_') == 2 && s.count('|') == 0, // the mirror
@@ -61,15 +61,21 @@ const categories = [
       f: (s) => s.count('_') >= 4 && s.count('|') == 0, // Four cracks
     },
     {
-      name: "Curvy Lines",
-      text: ["Scattered curvy paths with unclear direction or tendency. A decision needs to be made so that the right path shall reveal."],
-      detections: [],
-      f: (s) => s.count('+') >= 2 && s.count('|') >= 1, // branches
-    },
-    {
       name: "The Serpentine",
       text: ["A long vertical crack winding its way on the edge, speaks of hidden truths and secrets waiting to be revealed."],
       detections: [],
       f: (s) => s.count('|') == 1, // branches
+    },
+    {
+      name: "The Teller",
+      text: ["A clear single crack that opens like a telling mouth, unspoken words need to be told and heard. The bigger the opening, the more needs to be shared."],
+      detections: [],
+      f: (s) => s.count('>') >= 1, // The Teller
+    },
+    {
+      name: "The Indecisive Heart",
+      text: ["Multiple half cracks that don't crack through the whole grain, a decision needs to be made so that the right path shall reveal."],
+      detections: [],
+      f: (s) => s.count('-') >= 3, // The Indecisive Heart
     }
   ]
